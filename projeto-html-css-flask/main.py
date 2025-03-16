633e95ccd7e69aab07778796e99bff9014137992
BernardoLCB <bernardoleinig@gmail.com>
2025-03-16 16:45:32 -0300

    main.py

--- projeto-html-css-flask/main.py
+++ projeto-html-css-flask/main.py
@@ -2,12 +2,7 @@ from flask import Flask, render_template, request
 import dates
 import random
 
-
-
 app = Flask(__name__)
-
-palavras = ["Python", "Flask", "Programação", "Desenvolvimento", "Back-end", "Web", "API", "Servidor", "Banco de Dados", "HTML"]
-
 
 @app.route('/', methods=['GET', 'POST'])
 def principal():
@@ -15,10 +10,8 @@ def principal():
     if request.method == 'POST':
         randon_numb_list = random.randint(0,3)
         randon_numb_world = random.randint(1,100)
-
         lista = dates.searchWord(randon_numb_list,randon_numb_world)
- 
-    # lista = dates.searchWord() if request.method == 'POST' else "Clique no botão para gerar uma palavra"
+        
     return render_template("index.html", lista=lista)
 
 
